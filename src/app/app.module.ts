1f23a3160a69dd56adf8acdb78cf4b941b90270c
Marcin Siedlarz <marcin.siedlarz@joymile.com>
2020-01-19 21:39:41 +0100

    feat: add skeleton of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
